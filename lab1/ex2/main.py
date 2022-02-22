10d378594db2f708f46d48402dc8846a2836d794
Projeos AW <projetos@awdigital.com.br>
2022-02-22 15:54:38 -0300

    ex2 feito

--- lab1/ex2/main.py
+++ lab1/ex2/main.py
@@ -2,20 +2,29 @@ current_index = 0
 current_state = "q0"
 
 def filter_word(ex_accepted_char, word):
-  for i in range(len(ex_accepted_char)):
-    if word[i] != ex_accepted_char[i]:
-      accepted = False
-    else:
-      accepted= True
 
+  accepted = False
+
+  if word != "":
+    for i in range(len(ex_accepted_char)):
+      if word[i] != ex_accepted_char[i]:
+        accepted = False
+      else:
+        accepted= True
+    
   return accepted
 
 def main():
 
   word = input(str("Digite a palavra: "))
   
-  accepted = filter_word(["i", "f"], word)
-  #accepted = filter_word(["e", "l", "s", "e"], word)
+  words = [["i", "f"], ["f", "o", "r"], ["e", "l", "s", "e"], ["t", "h", "e", "n"]]
+
+  for i in range(len(words)):
+    accepted = filter_word(words[i],word)
+
+    if accepted == True:
+      break
 
   if accepted:
     print("AFD aceito")
