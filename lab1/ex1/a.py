751e437c0d86df067406fb292d97eba0f139ed0a
Projeos AW <projetos@awdigital.com.br>
2022-02-22 12:54:42 -0300

    ex2 feito

--- lab1/ex1/a.py
+++ lab1/ex1/a.py
@@ -4,17 +4,14 @@ ex_accepted_char = ["0", "1", "0"]
 accepted = True
 
 word = input(str("Digite a palavra: "))
-print(type(word))
 
-if len(word) != 3:
+if len(word) != len(ex_accepted_char):
   accepted = False
 
 if accepted:
   for i in range(len(word)):
     if word[i] != ex_accepted_char[i]:
       accepted = False
-    
-print(accepted)
 
 if accepted:
   print("palavara %s aceita" %word)
---
+++ lab1/ex1/b.py
@@ -0,0 +1,20 @@
+current_char = ""
+accepted = True
+ex_accepted_char = []
+
+word = input(str("Digite a palavra: "))
+
+for i in range(len(word)):
+  if word[0] == "0":
+    ex_accepted_char.append("0")
+  else:
+    ex_accepted_char.append("1")
+
+for j in range(len(word)):
+  if word[j] != ex_accepted_char[j]:
+    accepted = False
+
+if accepted:
+  print("palavara %s aceita" %word)
+else:
+  print("palavra %s nao aceita" %word)
