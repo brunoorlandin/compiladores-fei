d04fa5dd1e586cba0081aa70ba59a380b94d26ca
Projeos AW <projetos@awdigital.com.br>
2022-02-22 15:46:26 -0300

    ex2 em andamento

--- lab1/ex2/main.py
+++ lab1/ex2/main.py
@@ -1,6 +1,28 @@
 current_index = 0
 current_state = "q0"
-accepted = True
-ex_accepted_char = []
 
-word = input(str("Digite a palavra: "))
+def filter_word(ex_accepted_char, word):
+  for i in range(len(ex_accepted_char)):
+    if word[i] != ex_accepted_char[i]:
+      accepted = False
+    else:
+      accepted= True
+
+  return accepted
+
+def main():
+
+  word = input(str("Digite a palavra: "))
+  
+  accepted = filter_word(["i", "f"], word)
+  #accepted = filter_word(["e", "l", "s", "e"], word)
+
+  if accepted:
+    print("AFD aceito")
+    print("palavra: %s" %word)
+  else:
+    print("AFD nao aceito")
+    print("palavra: %s" %word)
+
+if __name__ == "__main__":
+  main()
